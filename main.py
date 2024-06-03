b08fe2e46ac15a739b739a23e6d23a53e18de281
Eduardo Melo <168614510+eduardomelog@users.noreply.github.com>
2024-06-02 18:08:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,9 +27,6 @@ def get_recommendations(title, cosine_sim=similarity_matrix):
 
     # Devolvemos el listado
     return df2['title'].iloc[movie_indices]
-# END FIRST PAGE SETTINGS
-
-
 
 def get_recommendations2(title, cosine_sim=similarity_matrix):
     # Obtiene el indice que coincide con el título
@@ -53,6 +50,44 @@ def get_recommendations2(title, cosine_sim=similarity_matrix):
     df['Score'] = [str(round(x[1] * 100, 2)) + '%' for x in sim_scores]
     df.index = [1,2,3,4,5]
     return df
+# END FIRST PAGE SETTINGS
+
+
+
+
+
+
+# SECOND PAGE SETTINGS
+from surprise import Reader, Dataset, SVD
+from surprise.model_selection import cross_validate
+reader = Reader()
+ratings = pd.read_csv('ratings.csv')
+ratings.head()
+
+data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
+
+svd = SVD()
+#cross_validate(svd, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
+trainset = data.build_full_trainset()
+svd.fit(trainset)
+
+def recomendacion (uid):
+  lista_no_vistos = ratings[~ratings['movieId'].isin(ratings[ratings['userId'] == uid].movieId)][['movieId','original_title']].drop_duplicates()
+  recomendaciones = []
+  for ind in lista_no_vistos.index:
+    prediction= svd.predict(uid, lista_no_vistos['movieId'][ind])
+    pred_item, pred_est = prediction.iid, prediction.est
+    recomendacion = [pred_item, pred_est]
+    recomendaciones.append(recomendacion)
+  recomendaciones = pd.DataFrame(recomendaciones, columns=['movieId','rating_est'])
+  recomendaciones = recomendaciones.merge(lista_no_vistos).sort_values(by='rating_est', ascending=False)
+  return recomendaciones.head(5)
+# EN SECOND PAGE SETTINGS
+
+
+
+
 
 
 
@@ -64,11 +99,11 @@ st.image('movies.jpg',
              )
 
 # Crea las pestañas
-tab1, tab2 = st.tabs(["Content Based Model", "Pestaña 2"])
+tab1, tab2 = st.tabs(["Content Based Model", "Collaborative Filtering"])
 
 # Contenido de la Pestaña 1
 with tab1:
-    st.write('This model was made using the cosine similarity between some movie features such as the review, cast, gender and director.')
+    st.header('This model was made using the cosine similarity between some movie features such as the review, cast, gender and director.')
     movie = st.selectbox('Select a movie', sorted(df2['title']))
     if st.button('Recommend Movies'):
         df = get_recommendations2(movie)
@@ -78,6 +113,5 @@ with tab1:
 # Contenido de la Pestaña 2
 with tab2:
     st.header("Bienvenido a la Pestaña 2")
-    st.write("Este es el contenido de la segunda pestaña.")
-    st.line_chart({"data": [1, 5, 2, 6, 8, 3]})
+    st.write(recomendacion(1))
     
